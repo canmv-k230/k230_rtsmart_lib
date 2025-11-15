3c5e3d6376c7788ee68368cf6502da980eca0268
kendryte747 <kendryte747@gmail.com>
2025-11-15 21:35:10 +0800

    refactor(lvgl): improve display driver architecture and buffer handling
    Reorganize the K230 LVGL display driver for better maintainability and clarity. Key changes include:
    - Refactored function naming for consistency (configure vs reconfigure)
    - Simplified buffer management by removing unused fields and consolidating related data
    - Added helper functions to eliminate code duplication in buffer calculations and setup
    - Improved DMA rotation handling with cleaner parameter passing
    - Enhanced function organization with better grouping and forward declarations
    - Fixed file path handling in Makefile using patsubst to remove absolute prefixes

--- mk/lib3rdparty.mk
+++ mk/lib3rdparty.mk
@@ -1,9 +1,6 @@
 lib3rd_party_inc_dir := $(SDK_RTSMART_BUILD_DIR)/libs/3rd-party/include
 lib3rd_party_lib_dir := $(SDK_RTSMART_BUILD_DIR)/libs/3rd-party/lib
 
-lib3rd_party_inc_dir += $(SDK_RTSMART_SRC_DIR)/libs/3rd-party/lvgl/port
-lib3rd_party_inc_dir += $(SDK_RTSMART_SRC_DIR)/libs/3rd-party/lvgl/lvgl
-
 LIB_CFLAGS += $(addprefix -I, $(lib3rd_party_inc_dir))
 LIB_LDFLAGS += $(addprefix -L, $(lib3rd_party_lib_dir)) 
 LIB_LDFLAGS += -Wl,--start-group $(addprefix -l,$(subst lib, ,$(basename $(notdir $(foreach dir, $(lib3rd_party_lib_dir), $(wildcard $(dir)/*)))))) -Wl,--end-group
