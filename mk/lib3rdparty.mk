3b8b547282bb3e0c5d668d348b63ffe1fe8c53f4
kendryte747 <kendryte747@gmail.com>
2026-04-29 11:18:47 +0800

    feat(makefiles): streamline header installation process and update Makefile structure

--- mk/lib3rdparty.mk
+++ mk/lib3rdparty.mk
@@ -1,6 +1,9 @@
 lib3rd_party_inc_dir := \
 	$(SDK_RTSMART_BUILD_DIR)/libs/3rd-party/include \
 	$(SDK_RTSMART_BUILD_DIR)/libs/3rd-party/include/cJSON \
+	$(SDK_RTSMART_BUILD_DIR)/libs/3rd-party/include/freetype \
+	$(SDK_RTSMART_BUILD_DIR)/libs/3rd-party/include/mbedtls \
+	$(SDK_RTSMART_BUILD_DIR)/libs/3rd-party/include/minihttp \
 
 lib3rd_party_lib_dir := $(SDK_RTSMART_BUILD_DIR)/libs/3rd-party/lib
 
