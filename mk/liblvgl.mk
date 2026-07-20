a68d15c361aabb9ef7447881a04780778b952e06
kendryte747 <kendryte747@gmail.com>
2026-07-20 11:37:31 +0800

    fix(lvgl): adjust library linking for FreeType support

--- mk/liblvgl.mk
+++ mk/liblvgl.mk
@@ -6,4 +6,8 @@ liblvgl_lib_dir := $(SDK_RTSMART_BUILD_DIR)/libs/3rd-party/lib
 
 LIB_CFLAGS += $(addprefix -I, $(liblvgl_inc_dir))
 LIB_LDFLAGS += $(addprefix -L, $(liblvgl_lib_dir)) 
-LIB_LDFLAGS += -Wl,--start-group -llvgl -Wl,--end-group
+LIB_LDFLAGS += -Wl,--start-group -llvgl
+ifeq ($(CONFIG_RTSMART_3RD_PARTY_ENABLE_FREETYPE),y)
+LIB_LDFLAGS += -lfreetype
+endif
+LIB_LDFLAGS += -Wl,--end-group
